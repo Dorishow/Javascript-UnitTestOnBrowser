90bbdf4ce5f98e89dfa82d1b53f42dd56453d0e8
Dorishow <jsantosalmeida54@gmail.com>
2022-04-19 20:31:57 -0300

    setup: initial commit

---
+++ test.js
@@ -0,0 +1,72 @@
+/**
+ * Assertions object contains all test methods */
+const Assertions = {
+  /**
+   * @param {string} description
+   * @param {()=>void} callBackFn
+   */
+  it: (description, callBackFn) => {
+    try {
+      const assertion = callBackFn();
+      console.log(description, assertion);
+    } catch (error) {
+      console.error(description, error);
+    }
+  },
+
+  /**
+   * @param {*} var1
+   * @param {*} var2
+   * @returns string or else throw error
+   */
+  assertEquals: (var1, var2) => {
+    if (var1 !== var2) {
+      throw new Error(`${var1} is not equals to ${var2}`);
+    } else {
+      return `Test passed: ${var1} equals to ${var2}`;
+    }
+  },
+
+  /**
+   * @param {*} var1
+   * @param {*} var2
+   * @returns string or else throw error
+   */
+  assertNotEquals: (var1, var2) => {
+    if (var1 === var2) {
+      throw new Error(`${var1} and ${var2} are equals, but expected not`);
+    } else {
+      return `Test passed: ${var1} and ${var2} are not equals`;
+    }
+  },
+
+  /**
+   *
+   * @param {*} var1
+   * @returns string or else throw error
+   */
+  notNull: (var1) => {
+    if (var1 === null) {
+      throw new Error(`${var1} is null`);
+    } else {
+      return `Test passed: ${var1} is not null`;
+    }
+  },
+};
+
+/**
+ * set Assertions to global window object */
+window.Assertions = Assertions;
+
+console.table(window.Assertions);
+
+/**
+ * Examples
+ *
+ * Assertions.it("should throw error", () => Assertions.assertEquals(1, 2));
+ *
+ * Assertions.it("should pass", () => Assertions.assertEquals(1, 1));
+ *
+ */
+
+Assertions.assertEquals;
